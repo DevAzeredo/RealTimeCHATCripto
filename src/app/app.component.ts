418f6e948bf4d1b529329e0e7c2b5c1e4adfd2d9
DevAzeredo <developer.azeredo@gmail.com>
2024-02-04 00:46:27 -0300

    embelezando chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ChatComponent]
+    imports: [RouterOutlet, ChatComponent, FormsModule]
 })
 export class AppComponent {
   title = 'RealTimeCHATFrontEnd';
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,49 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Client, IMessage, StompConfig, Versions } from '@stomp/stompjs';
 import SockJS from 'sockjs-client/dist/sockjs';
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
   private client: Client = new Client;
-  public messages: string[] = [];
+  public inMessages: string[] = [];
+  outMessage: string = '';
+  crypt: string = '';
   constructor() {
+    this.inMessages = [];
   }
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    this.setUpWebSocketClient();
+  }
+
+  setUpWebSocketClient() {
     const stompConfig: StompConfig = {
       stompVersions: new Versions(['1.1', '1.0']),
       webSocketFactory: () => {
         return new SockJS('http://localhost:8080/ws');
       }
     };
-
     this.client = new Client(stompConfig);
+    this.client.onConnect = () => {
+      console.log('Conexão estabelecida com sucesso. Inscrevendo-se...');
+      this.client.subscribe('/topic/public', (message: any) => {
+        this.onMessageReceived(message)
+        this.inMessages.push(JSON.parse(message.body).content);
+      });
+    };
     this.client.activate();
-    this.client.subscribe('/topic/public', this.onMessageReceived);
-  }
-  onMessageReceived(message: IMessage): void {
-    console.log('deu bom:',message);
   }
 
-
+  onMessageReceived(message: IMessage): string {
+    return JSON.parse(message.body).content
+  }
 
   sendMessage() {
-    console.log('deu sss:');
-
     var chatMessage = {
-      Chat:1,
-      content: "mensagme que enviei do angular",
-      Id:1
-
-  };
-
-    this.client.publish({ destination: "/app/sendMessage", body: JSON.stringify(chatMessage)});
+      Chat: 1,
+      content: this.outMessage,
+      Id: 1
+    };
+    this.client.publish({ destination: "/app/sendMessage", body: JSON.stringify(chatMessage) });
+    this.outMessage = '';
   };
 }
 
